0b89d720741b3b0c79c12d0a02d386839986b219
RolandYambao <rolandyambao@gmail.com>
2021-11-08 12:03:42 -0800

    In Progress

--- app.js
+++ app.js
@@ -1,61 +1,36 @@
-let searchOne = document.getElementById("hidingSpot1");
-let searchTwo = document.getElementById("hidingSpot2");
-let searchThree = document.getElementById("hidingSpot3");
-let searchFour = document.getElementById("hidingSpot4");
-let searchFive = document.getElementById("hidingSpot5");
-let searchSix = document.getElementById("hidingSpot6");
-let searchSeven = document.getElementById("hidingSpot7");
-let searchEight = document.getElementById("hidingSpot8");
-let searchNine = document.getElementById("hidingSpot9");
-let searchTen = document.getElementById("hidingSpot10");
-let searchEleven = document.getElementById("hidingSpot11");
-let searchTwelve = document.getElementById("hidingSpot12");
-let searchThirteen = document.getElementById("hidingSpot13");
-let searchFourteen = document.getElementById("hidingSpot14");
-let searchFifteen = document.getElementById("hidingSpot15");
+let hidingSpot = document.getElementsByClassName("hidingSpot");
+let intelligence = document.getElementById("intelligence");
 
-searchOne.addEventListener("click", function () {
+/*
+hidingSpot[0].addEventListener("click", function () {
     alert("Area 1 has been Searched");
 })
-searchTwo.addEventListener("click", function () {
-    alert("Area 2 has been Searched");
-})
-searchThree.addEventListener("click", function () {
-    alert("Area 3 has been Searched");
-})
-searchFour.addEventListener("click", function () {
-    alert("Area 4 has been Searched");
-})
-searchFive.addEventListener("click", function () {
-    alert("Area 5 has been Searched");
-})
-searchSix.addEventListener("click", function () {
-    alert("Area 6 has been Searched");
-})
-searchSeven.addEventListener("click", function () {
-    alert("Area 7 has been Searched");
-})
-searchEight.addEventListener("click", function () {
-    alert("Area 8 has been Searched");
-})
-searchNine.addEventListener("click", function () {
-    alert("Area 9 has been Searched");
-})
-searchTen.addEventListener("click", function () {
-    alert("Area 10 has been Searched");
-})
-searchEleven.addEventListener("click", function () {
-    alert("Area 11 has been Searched");
-})
-searchTwelve.addEventListener("click", function () {
-    alert("Area 12 has been Searched");
-})
-searchThirteen.addEventListener("click", function () {
-    alert("Area 13 has been Searched");
-})
-searchFourteen.addEventListener("click", function () {
-    alert("Area 14 has been Searched");
-})
-searchFifteen.addEventListener("click", function () {
-    alert("Area 15 has been Searched");
-})
+*/
+
+function watchtowerSearch() {
+    hidingSpot[0].addEventListener("click", function () {
+        alert("Area 1 has been Searched");
+        return 1;
+    })
+    hidingSpot[1].addEventListener("click", function () {
+        alert("Area 2 has been Searched");
+        return 2;
+    })
+    hidingSpot[2].addEventListener("click", function () {
+        alert("Area 3 has been Searched");
+        return 3;
+    })
+    hidingSpot[3].addEventListener("click", function () {
+        alert("Area 3 has been Searched");
+        return 4;
+    })
+}
+
+function aiTroublemaker() {
+    let randomNumber = Math.floor(Math.random() * 15 + 1)
+    console.log(randomNumber);
+    if (randomNumber == watchtowerSearch()) {
+        alert("You found the Troublemaker");
+    }
+}
+aiTroublemaker();

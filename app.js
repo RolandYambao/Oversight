d22fd7cb1106bd41a1be12eccfa9086155ad42ec
RolandYambao <rolandyambao@gmail.com>
2021-11-09 14:39:29 -0800

    Updated

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function sixTurns() {
     }
 }
 function successfulMission() {
+    turns = 6;
     alert("You found the Troublemaker");
     music.setAttribute("src", "tisForThee.mp3");
     music.volume = 0.5;
@@ -39,17 +40,17 @@ function failedMission() {
     sixTurns();
 }
 function intel() {
-    if (turns >= 6){
+    if (turns >= 6) {
         location.reload();
     }
     if (turns < 1) {
         intelligence.innerText = "Scour the area, fire 3 shots to find the troublemaker!";
-    }  
+    }
     if (turns > 1 && turns < 4) {
         if (computerChoice == 1 || computerChoice == 2 || computerChoice == 6 || computerChoice == 11 || computerChoice == 12) {
             if (guess == 1) {
                 intelligence.innerText = "Report: The troublemaker is not in the Central Areas";
-            } else  if (guess == 2) {
+            } else if (guess == 2) {
                 intelligence.innerText = "Report: The troublemaker is not in the Central Areas";
             } else {
                 intelligence.innerText = "Report: The troublemaker is not in the Eastern Areas";
@@ -58,7 +59,7 @@ function intel() {
         if (computerChoice == 4 || computerChoice == 5 || computerChoice == 10 || computerChoice == 14 || computerChoice == 15) {
             if (guess == 1) {
                 intelligence.innerText = "Report: The troublemaker is not in the Western Areas";
-            } else  if (guess == 2) {
+            } else if (guess == 2) {
                 intelligence.innerText = "Report: The troublemaker is not in the Central Areas";
             } else {
                 intelligence.innerText = "Report: The troublemaker is not in the Western Areas";
@@ -67,7 +68,7 @@ function intel() {
         if (computerChoice == 3 || computerChoice == 7 || computerChoice == 8 || computerChoice == 9 || computerChoice == 13) {
             if (guess == 1) {
                 intelligence.innerText = "Report: The troublemaker is not in the Western Areas";
-            } else  if (guess == 2) {
+            } else if (guess == 2) {
                 intelligence.innerText = "Report: The troublemaker is not in the Eastern Areas";
             } else {
                 intelligence.innerText = "Report: The troublemaker is not in the Eastern Areas";
@@ -95,6 +96,8 @@ function watchtowerSearch() {
         hidingSpot[0].style.backgroundSize = "100% 100%";
         console.log("Area 1 has been Searched");
         if (computerChoice == 1) {
+            hidingSpot[0].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[0].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -106,6 +109,8 @@ function watchtowerSearch() {
         hidingSpot[1].style.backgroundSize = "100% 100%";
         console.log("Area 2 has been Searched");
         if (computerChoice == 2) {
+            hidingSpot[1].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[1].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -117,6 +122,8 @@ function watchtowerSearch() {
         hidingSpot[2].style.backgroundSize = "100% 100%";
         console.log("Area 3 has been Searched");
         if (computerChoice == 3) {
+            hidingSpot[2].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[2].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -128,6 +135,8 @@ function watchtowerSearch() {
         hidingSpot[3].style.backgroundSize = "100% 100%";
         console.log("Area 4 has been Searched");
         if (computerChoice == 4) {
+            hidingSpot[3].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[3].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -139,6 +148,8 @@ function watchtowerSearch() {
         hidingSpot[4].style.backgroundSize = "100% 100%";
         console.log("Area 5 has been Searched");
         if (computerChoice == 5) {
+            hidingSpot[4].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[4].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -150,6 +161,8 @@ function watchtowerSearch() {
         hidingSpot[5].style.backgroundSize = "100% 100%";
         console.log("Area 6 has been Searched");
         if (computerChoice == 6) {
+            hidingSpot[5].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[5].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -161,6 +174,8 @@ function watchtowerSearch() {
         hidingSpot[6].style.backgroundSize = "100% 100%";
         console.log("Area 7 has been Searched");
         if (computerChoice == 7) {
+            hidingSpot[6].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[6].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -172,6 +187,8 @@ function watchtowerSearch() {
         hidingSpot[7].style.backgroundSize = "100% 100%";
         console.log("Area 8 has been Searched");
         if (computerChoice == 8) {
+            hidingSpot[7].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[7].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -183,6 +200,8 @@ function watchtowerSearch() {
         hidingSpot[8].style.backgroundSize = "100% 100%";
         console.log("Area 9 has been Searched");
         if (computerChoice == 9) {
+            hidingSpot[8].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[8].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -194,6 +213,8 @@ function watchtowerSearch() {
         hidingSpot[9].style.backgroundSize = "100% 100%";
         console.log("Area 10 has been Searched");
         if (computerChoice == 10) {
+            hidingSpot[9].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[9].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -205,6 +226,8 @@ function watchtowerSearch() {
         hidingSpot[10].style.backgroundSize = "100% 100%";
         console.log("Area 11 has been Searched");
         if (computerChoice == 11) {
+            hidingSpot[10].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[10].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -216,6 +239,8 @@ function watchtowerSearch() {
         hidingSpot[11].style.backgroundSize = "100% 100%";
         console.log("Area 12 has been Searched");
         if (computerChoice == 12) {
+            hidingSpot[11].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[11].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -227,6 +252,8 @@ function watchtowerSearch() {
         hidingSpot[12].style.backgroundSize = "100% 100%";
         console.log("Area 13 has been Searched");
         if (computerChoice == 13) {
+            hidingSpot[12].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[12].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -238,6 +265,8 @@ function watchtowerSearch() {
         hidingSpot[13].style.backgroundSize = "100% 100%";
         console.log("Area 14 has been Searched");
         if (computerChoice == 14) {
+            hidingSpot[13].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[13].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
@@ -249,6 +278,8 @@ function watchtowerSearch() {
         hidingSpot[14].style.backgroundSize = "100% 100%";
         console.log("Area 15 has been Searched");
         if (computerChoice == 15) {
+            hidingSpot[14].style.backgroundImage = "url(Bullseye.png)";
+            hidingSpot[14].style.backgroundSize = "100% 100%";
             successfulMission();
         } else {
             failedMission();
